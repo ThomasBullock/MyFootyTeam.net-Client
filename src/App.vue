316f17d5219a3a257a95a34ce4ccf36e4b9dacff
Thomas Bullock <motbollox@gmail.com>
2017-05-19 21:15:09 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+ 
+    <h1 class="display-3">Hello, world!</h1>
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/bootstrap-4.0.0-alpha.6/scss/bootstrap-custom.scss";
+  @import './assets/scss/main.scss';
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+//import Bootstrap from './assets/bootstrap-4.0.0-alpha.6/js/dist/dropdown' // importing bootstrap.js
+//import "./assets/bootstrap-4.0.0-alpha.6/scss/bootstrap.scss"
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
